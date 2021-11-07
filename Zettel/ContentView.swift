41179a5efa2eb144440e1f29e9b057661e8552b6
Simon Lang <simonmilagrolang@icloud.com>
2021-11-07 10:10:59 +0100

    Initial Commit

---
+++ Zettel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zettel
+//
+//  Created by Simon Lang on 07.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zettel/ZettelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZettelApp.swift
+//  Zettel
+//
+//  Created by Simon Lang on 07.11.21.
+//
+
+import SwiftUI
+
+@main
+struct ZettelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
