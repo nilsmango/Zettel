3330726cb518318a3901b9149e2f1ba2068ac40c
nilsmango <simonmilagrolang@icloud.com>
2024-12-01 09:37:24 +0100

    Removed mac app

---
+++ Zettel/ZettelMenu.swift
@@ -0,0 +1,18 @@
+//
+//  ZettelMenu.swift
+//  Zettel
+//
+//  Created by Simon Lang on 01.12.2024.
+//
+
+import SwiftUI
+
+struct ZettelMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ZettelMenu()
+}
